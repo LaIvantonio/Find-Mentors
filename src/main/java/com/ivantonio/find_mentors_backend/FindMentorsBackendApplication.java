5f17a4145451d63d768de3fb6c07cd8a02182448
Ivan Borodinov <ivan.borodinov.03@mail.ru>
2024-08-11 13:14:42 +0300

    start a new project

---
+++ src/main/java/com/ivantonio/find_mentors_backend/FindMentorsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ivantonio.find_mentors_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMentorsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMentorsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivantonio/find_mentors_backend/FindMentorsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivantonio.find_mentors_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMentorsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
